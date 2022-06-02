b41a4448efe08db054058a4d965c28df0e9784c9
codetobas999 <codetobas999@gmail.com>
2022-06-02 22:02:12 +0700

    codetobas999@gmail.com

---
+++ LABS/IOT/Drivers/Test_LED.py
@@ -0,0 +1,24 @@
+from machine import Pin 
+led14 = Pin(14,Pin.OUT)
+led13 = Pin(13,Pin.OUT)
+led12 = Pin(12,Pin.OUT)
+from machine import Pin
+import time
+
+led14 = Pin(14,Pin.OUT)
+led13 = Pin(13,Pin.OUT)
+led12 = Pin(12,Pin.OUT)
+for i in range(1000):
+    print('Index :' + str(i))
+    led14.on()
+    led13.off()
+    led12.off()
+    time.sleep(0.5)
+    led14.off()
+    led13.on()
+    led12.off()
+    time.sleep(0.5) 
+    led14.off()
+    led13.off()
+    led12.on()
+    time.sleep(0.5) 

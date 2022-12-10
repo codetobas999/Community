ef9d65697f8e4c386d4994af1db7e4bdb348bdb0
codetobas999 <codetobas999@gmail.com>
2022-12-11 03:46:38 +0700

    commit

--- PROJECTS/proj-cute-pets/backend/app/api/api_v1/handlers/auth_group.py
+++ PROJECTS/proj-cute-pets/backend/app/api/api_v1/handlers/auth_group.py
@@ -1,8 +1,11 @@
-from fastapi import APIRouter  
+from fastapi import APIRouter , Depends
+from app.models.user_model import User
 from app.services.auth_group_service import AuthGroupService 
+from app.api.deps.user_deps import get_current_user
 
 auth_group_router = APIRouter()
 @auth_group_router.get('/',summary="Get all pages of the user")
-async def list_authen_by_group(): 
-    group_code = "G0001"
-    return  await AuthGroupService.list_authen_by_group(group_code)
+async def list_authen_by_group(current_user: User =  Depends(get_current_user)): 
+    user_code = current_user.email
+    print(user_code)
+    return  await AuthGroupService.list_authen_by_group(user_code)
--- PROJECTS/proj-cute-pets/backend/app/services/auth_group_service.py
+++ PROJECTS/proj-cute-pets/backend/app/services/auth_group_service.py
@@ -3,10 +3,27 @@ from app.models.page_model import Page
 
 class AuthGroupService:
     @staticmethod
-    async def list_authen_by_group(group_code):         
-        print("list_authen_by_group 2" + group_code)  
-        stage_row_filter1 = {'$match': {'group_code':{"$in":[group_code]}}}
-        stage_lookup2 = {
+    async def list_authen_by_group(user_code):         
+        print("list_authen_by_group 2" + user_code)  
+        stage_lookup_AuthUser = {
+                "$lookup": {
+                    "from": "AuthUserGroup", 
+                    "let": {"groupCode":"$group_code"},  
+                    "pipeline": [{
+                            "$match": {
+                                "$expr": {
+                                    "$and": [
+                                            { "$eq": ["$group_code", "$$groupCode"]}, 
+                                            { "$eq": ["$user_code", user_code]}
+                                    ]            
+                                }
+                            }
+                    }],
+                    "as": "group"
+                 }
+        }
+
+        stage_lookup_AuthPage = {
                 "$lookup": {
                     "from": "AuthPage", 
                     "let": {"pageCode":"$page_code"},  
@@ -23,7 +40,7 @@ class AuthGroupService:
                     "as": "page"
                  }
         }
-        stage_lookup3 = {
+        stage_lookup_AuthPageSub = {
                 "$lookup": {
                     "from": "AuthPageSub", 
                     "let": {"pageCode":"$page_code" , "pageSubCode":"$page_sub_code" }, 
@@ -42,47 +59,19 @@ class AuthGroupService:
                  }
         }
         
-        stage_unset4 = { "$unset": [ "_id" , "group_code" ,"page_code" ,"page_sub_code" ,"status", "page._id", "page_subs._id"    ] }                
-        stage_unwind5 = { "$unwind": "$page" }
-
-        stage_limit = { "$limit": 20 }
-        stage_field_filter = { "$project" : { "_id": 0, "pages._id": 0 , "page_subs._id": 0 }  } #hide Column
-        stage_row_filter = {'$match': {'status': True}}
+        stage_hide_fiels = { "$unset": [ "_id" , "group_code" ,"page_code" ,"page_sub_code" ,"status", "page._id", "page_subs._id"    ] }                
+        stage_unarray_page = { "$unwind": "$page" }
+        stage_unarray_group = { "$unwind": "$group" }
+        stage_field_filter = { "$project" : { "_id": 0 , "group._id": 0 , "pages._id": 0 , "page_subs._id": 0 }  } #hide Column
         pipeline = [
-            stage_row_filter1,
-            stage_lookup2,
-            stage_unwind5,
-            stage_lookup3,
-            #stage_limit, 
+            stage_lookup_AuthUser,
+            stage_unarray_group,
+            stage_lookup_AuthPage,
+            stage_unarray_page,
+            stage_lookup_AuthPageSub, 
             stage_field_filter,
-            stage_unset4,
-            
-            #stage_row_filter
+            stage_hide_fiels, 
         ]
  
         todos= AuthGroup.aggregate(pipeline)
         return await todos.to_list()
-
-        '''
-        stage_lookup = {
-                "$lookup": {
-                    "from": "PageSub", 
-                    "localField": "page_code", 
-                    "foreignField": "page_code", 
-                    "as": "pages",
-                 }
-        }
-        stage_limit = { "$limit": 20 }
-        stage_field_filter = { "$project" : { "_id": 0, "pages._id": 0 }  } #hide Column
-        stage_row_filter = {'$match': {'status': True}}
-        pipeline = [
-            stage_lookup,
-            stage_limit,
-            stage_field_filter,
-            stage_row_filter
-        ]
- 
-        todos= Page.aggregate(pipeline)
-        
-        return await todos.to_list()
-        '''
